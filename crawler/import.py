f8073835ccf2795d5e8ed5d30ce565c3bebeee64
Weikun Wu <weikunw@hotmail.com>
2022-10-21 19:29:10 -0500

    Add panda to remove duplicate

--- crawler/import.py
+++ crawler/import.py
@@ -3,12 +3,13 @@ import re
 from datetime import date, datetime
 from datetime import timedelta
 import hashlib
+import pandas as pd
 
 def processRawTicket(rawTicket):
     ticketDates = re.findall("Ticket Date: \d{1,2}-\d{1,2}-\d{4}", rawTicket)
     rawticketDate = ticketDates[0].split(" ")[-1]
     ticketDateObject = datetime.strptime(rawticketDate, "%m-%d-%Y")
-    ticketDate = ticketDateObject.strftime("%m-%d-%Y")
+    ticketDate = ticketDateObject.strftime("%Y-%m-%d")
 
     departureDates = re.findall("Departure Date: \d{1,2}-\d{1,2}-\d{4}", rawTicket)
     rawdepartureDate = departureDates[0].split(" ")[-1]
@@ -25,21 +26,24 @@ def processRawTicket(rawTicket):
     departureDateObject = datetime.strptime(rawdepartureDate, "%m-%d-%Y")
     arrivalDateObject = departureDateObject if flightTimes[1][1] == "" else departureDateObject + timedelta(days=int(flightTimes[1][1].split("+")[-1]))
 
-
-    departureDate = departureDateObject.strftime("%m-%d-%Y")
-    arrivalDate = arrivalDateObject.strftime("%m-%d-%Y")
+    departureDate = departureDateObject.strftime("%Y-%m-%d")
+    arrivalDate = arrivalDateObject.strftime("%Y-%m-%d")
     arrivalTimeStr = flightTimes[1][0]
     arrivalTimeObject = datetime.strptime(arrivalTimeStr, "%I:%M %p")
     arrivalTime = arrivalTimeObject.strftime("%H:%M")
 
     travelTime = re.findall("Travel time: ((\d{1,2} hr)? ?(\d{1,2} min)?)", rawTicket)
 
-    # print("traveltime", travelTime)
     if len(travelTime) == 0:
         return None
-    travelTime = travelTime[0][0]
+    hr = 0
+    min = 0
+    if travelTime[0][1] != "":
+        hr = int(travelTime[0][1].split(" ")[0])
+    if travelTime[0][2] != "":
+        min = int(travelTime[0][2].split(" ")[0])
+    travelTime = str(timedelta(hours=hr, minutes=min))
     
-
     price = re.findall("\$\d+\,?\d+", rawTicket)
     if len(price) == 0:
         price = None
@@ -47,7 +51,7 @@ def processRawTicket(rawTicket):
         priceArr = price[0][1:].split(",")
         price = "".join(priceArr)
 
-    flightNumber = re.findall("[A-Z0-9]{2} \d{2,4}", rawTicket)
+    flightNumber = re.findall("([A-Z0-9]{2} \d{2,4})\n", rawTicket)
     if (len(flightNumber) == 0):
         return None
     flightNumber = flightNumber[0]
@@ -72,8 +76,6 @@ def processRawTicket(rawTicket):
 
     ticketID = hashlib.sha256((flightNumber + departureDate + ticketDate).encode())
     ticketID = ticketID.hexdigest()
-    # print(flightID)
-    # print(ticketID)
 
     result = {}
     result["departAirport"] = (departAirportCode, departAirportName)
@@ -85,8 +87,6 @@ def processRawTicket(rawTicket):
 inF = open("flight_data.txt")
 rawdata = inF.read()
 dataArray = rawdata.split("\n\nseperator\n\n")
-
-# print(processRawTicket(dataArray[0]))
 
 flightTable = open('../data/flight.csv', 'a')
 airportTable = open('../data/airport.csv', 'a')
@@ -100,14 +100,12 @@ flightFields = ["flight_id", "flight_number", "airline_code", "departure_date", 
 airportFields = ["IATA", "airport_name"]
 ticketFields = ["ticket_id", "flight_id", "purchase_date", "class", "price"]
 
+# processRawTicket(dataArray[0])
 flightWriter.writerow(flightFields)
 airportWriter.writerow(airportFields)
 ticketWriter.writerow(ticketFields)
 
-# processedData = processRawTicket(dataArray[0])
-
 airportSet= set()
-flightSet = set()
 for data in dataArray:
     # print("data",data)
     if (data == ""):
@@ -115,9 +113,7 @@ for data in dataArray:
     processedData = processRawTicket(data)
     if processedData is None:
         continue
-    if processedData["flight"][0] not in airportSet:
-        flightWriter.writerow(processedData["flight"])
-        flightSet.add(processedData["flight"][0])
+    flightWriter.writerow(processedData["flight"])
     if processedData["departAirport"][0] not in airportSet:
         airportWriter.writerow(processedData["departAirport"])
         airportSet.add(processedData["departAirport"][0])
@@ -125,3 +121,7 @@ for data in dataArray:
         airportWriter.writerow(processedData["arrivalAirport"])
         airportSet.add(processedData["arrivalAirport"][0])
     ticketWriter.writerow(processedData["ticket"])
+
+flights = pd.read_csv("../data/flight.csv")
+flights = flights.drop_duplicates()
+flights.to_csv("../data/flight_no_dup.csv",index=False)
