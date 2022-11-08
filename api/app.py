6476632506acf7627639ccbecdc4fdd2d885578f
weikunwu <35621622+weikunwu@users.noreply.github.com>
2022-11-08 17:56:03 -0600

    Comment branch
    * Add search to comment
    
    * I commit
    
    * add delete comment
    
    * comment.py
    
    * change to request.args.copy()
    
    * add id after comment/
    
    * add space to return
    
    Co-authored-by: Kurt Ji <cji10@illinois.edu>

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from service.airport import airport_bp
 from service.flight import flight_bp
 from service.ticket import ticket_bp
+from service.comment import comment_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +12,4 @@ CORS(app)
 app.register_blueprint(airport_bp, url_prefix="/api/")
 app.register_blueprint(flight_bp, url_prefix="/api/")
 app.register_blueprint(ticket_bp, url_prefix="/api/")
+app.register_blueprint(comment_bp, url_prefix="/api/")
---
+++ api/service/comment.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request, abort, jsonify
+from service.db import DB
+
+db = DB()
+
+comment_bp = Blueprint("comment", __name__)
+
+
+# get all comments
+@comment_bp.route("/comments")
+def get_comments():
+
+    return db.search('Comment')
+
+# search for certain comments
+@comment_bp.route("/comment")
+def search_comment():
+
+    colNames = db.getColumnNames("Comment")
+    for column in request.args.keys():
+        if column not in colNames:
+            return "Incorrect column names"
+
+    try:
+        return db.search('Comment', request.args.copy())
+    except:
+        return "Incorrect input"
+  
+# add a comment
+@comment_bp.route("/comment", methods=['POST'])
+def add_comments():
+
+    colNames = db.getColumnNames("Comment")
+
+    for colName in colNames:
+        if request.form.get(colName) is None and colName != "comment_id":
+            return "Missing " + colName
+
+    body = request.form.copy()
+
+    # print(request.form)
+    return db.insert("Comment", body)
+
+# delete comment
+@comment_bp.route("/comment/<comment_id>", methods=['DELETE'])
+def delete_comments(comment_id):
+
+    db.delete("Comment", {"comment_id": comment_id})
+
+    return "Comment id " + comment_id + " deleted"
