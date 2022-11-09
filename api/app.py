a7466ef082d77efa694263a0b76015dce49fc9c2
weikunwu <35621622+weikunwu@users.noreply.github.com>
2022-11-08 18:03:58 -0600

    Add airline route (#8)

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ from service.airport import airport_bp
 from service.flight import flight_bp
 from service.ticket import ticket_bp
 from service.comment import comment_bp
+from service.airline import airline_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -13,3 +14,4 @@ app.register_blueprint(airport_bp, url_prefix="/api/")
 app.register_blueprint(flight_bp, url_prefix="/api/")
 app.register_blueprint(ticket_bp, url_prefix="/api/")
 app.register_blueprint(comment_bp, url_prefix="/api/")
+app.register_blueprint(airline_bp, url_prefix="/api/")
